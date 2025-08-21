5264b41a74b77aed689ed336623b6d5026a765e7
Ahmed Mustafa <boskama134@gmail.com>
2025-08-21 14:56:30 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,29 @@
-from fastapi import FastAPI 
-from routes import base,data
+from fastapi import FastAPI
+from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
-# Create FastAPI application instance
 app = FastAPI()
 
-# Initialize MongoDB client on startup
 @app.on_event("startup")
 async def startup_db_client():
     settings = get_settings()
     app.mongodb_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongodb_conn[settings.MONGODB_DATABASE]
+    llm_factory = LLMProviderFactory(settings)
 
-# Close MongoDB client on shutdown    
+    # setup generation client
+    app.generation_client = llm_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+
+    # setup embedding client
+    app.embedding_client = llm_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID, embedding_size=settings.EMBEDDING_MODEL_SIZE)
+
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongodb_conn.close()
 
-# Include routers
 app.include_router(base.base_router)
 app.include_router(data.data_router)
-
