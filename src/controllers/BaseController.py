48a8c640480d9481c8ecb8048928f31d7d27aeaa
Ahmed Mustafa <boskama134@gmail.com>
2025-08-24 16:39:39 +0300

    Update BaseController.py

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -17,9 +17,25 @@ class BaseController:
             "assets/files"
         )
         
+        self.database_dir = os.path.join(
+            self.base_dir,
+            "assets/database"
+        )
+        
     # Generate a random string of specified length    
     def generate_random_string(self, length: int = 10):
         return ''.join(random.choices(
             string.ascii_lowercase + string.digits, k=length
         ))
         
+    def get_database_path(self, db_name: str):
+        
+        database_path = os.path.join(
+            self.database_dir, db_name
+        )
+        
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+            
+        return database_path
+    
